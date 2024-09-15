1d43946a43b5d05cf327e87d2712040ce26a2981
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-09-15 11:32:44 +0200

    Generate symmetric and nonsymmetric stable variable

--- stable.cpp
+++ stable.cpp
@@ -45,10 +45,50 @@ public:
     double generateExponentialNumber()
     {
         double number;
-        std::cout << uniform_distribution(generator) << std::endl;
         number = -std::log(uniform_distribution(generator));
 
         return number;
+    }
+
+    double generateSymmetricStableNumber()
+    {
+        double number;
+        double uniform = generateUniformNumber();
+
+        if (alpha == 1)
+        {
+            number = tan(uniform);
+
+            return number;
+        }
+        else
+        { 
+            double exponential = generateExponentialNumber();
+            number = (sin(alpha * uniform) / pow(cos(uniform), (1/alpha))) * pow(((cos((alpha - 1) * uniform))/ exponential), ((1-alpha)/alpha));
+
+            return number;
+        }
+    }
+
+    double generateNonSymmetricStableNumber()
+    {
+        double number;
+        double uniform = generateUniformNumber();
+        double exponential = generateExponentialNumber();
+
+        if (alpha == 1)
+        {
+            number = (2/std::numbers::pi) * (((std::numbers::pi/2) + (beta*uniform)) * tan(uniform) - beta * log(((std::numbers::pi/2 * exponential * cos(uniform)))/((std::numbers::pi/2) + beta*uniform)));
+
+            return number;
+        }     
+        else
+        {
+            double theta_0 = atan(beta*tan(std::numbers::pi*alpha/2)) / alpha;
+            number = (sin(alpha * (theta_0 + uniform)) / pow(cos(alpha*theta_0) * cos(uniform), (1/alpha))) * pow(((cos(alpha*theta_0 + (alpha - 1) * uniform))/ exponential), ((1-alpha)/alpha));
+
+            return number;
+        }
     }
 };
 
@@ -57,10 +97,13 @@ int main()
 {
     using std::cout, std::cin, std::endl;
 
-    StableDistribution* stable = new StableDistribution();
+    StableDistribution* stable = new StableDistribution(1.5,0,1,0);
 
-    std::cout << stable->generateExponentialNumber() << endl;
-    std::cout << stable->generateUniformNumber() << endl;
+    cout << stable->generateExponentialNumber() << endl;
+    cout << stable->generateUniformNumber() << endl;
+
+    cout << "Symmetric stable: " <<  stable->generateSymmetricStableNumber() << endl;
+    cout << "Nonsymmetric stable: " << stable->generateNonSymmetricStableNumber() << endl;
 
     return 0;
 }
