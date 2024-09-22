0c82ba50cef9d75353a08deff38bec134ff46e84
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-09-22 12:19:00 +0200

    Separate test cases

--- test/quantile_estimator_lookup_table_test.cpp
+++ test/quantile_estimator_lookup_table_test.cpp
@@ -29,38 +29,80 @@ BOOST_AUTO_TEST_CASE( QuantileEstimatorLookupTableConstructorValidation ) {
 }
 
 
-BOOST_FIXTURE_TEST_CASE( QuantileEstimatorTableCalculationDefaultSize, LookupTableFixture ) {
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorTableCalculationDefaultInitialization, LookupTableFixture ) {
     QuantileEstimatorLookupTable table;
-    size_t table_size;
-    size_t expected_size = 220;
-    table_size = getTableSize();
 
-    BOOST_CHECK_EQUAL(getAlphaMax(), 2.0);
-    BOOST_CHECK_EQUAL(getAlphaMin(), 0.1); 
-    BOOST_CHECK_EQUAL(getBetaMax(), 1.0);
-    BOOST_CHECK_EQUAL(getBetaMin(), 0.0);
     BOOST_CHECK_EQUAL(getMesh(), 0.1);
-
-    BOOST_CHECK_EQUAL(table_size, expected_size); 
-}
-
-
-BOOST_FIXTURE_TEST_CASE( QuantileEstimatorTableCalculationSize, LookupTableFixture ) {
-    QuantileEstimatorLookupTable table(0.25, 0.5, 2.0, 0.5, 1.0);
-    size_t table_size;
-    size_t expected_size;
-    table_size = getTableSize();
-    expected_size = (getAlphaMax() / getMesh()) *  ((getBetaMax() + getMesh()) / getMesh());
-
+    BOOST_CHECK_EQUAL(getAlphaMin(), 0.1); 
     BOOST_CHECK_EQUAL(getAlphaMax(), 2.0);
-    BOOST_CHECK_EQUAL(getAlphaMin(), 0.5); 
+    BOOST_CHECK_EQUAL(getBetaMin(), 0.0);
     BOOST_CHECK_EQUAL(getBetaMax(), 1.0);
-    BOOST_CHECK_EQUAL(getBetaMin(), 0.5);
-    BOOST_CHECK_EQUAL(getMesh(), 0.25);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorLookupTableMemberTableInitialziation, LookupTableFixture) {
+    QuantileEstimatorLookupTable table;
+
+    size_t table_size;
+    size_t expected_size = 4;
+    table_size = getTableSize();
 
     BOOST_CHECK_EQUAL(table_size, expected_size); 
 }
 
 
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorLookupTableCalculateVAlphaSize, LookupTableFixture) {
+    QuantileEstimatorLookupTable table;
+    table.calculateLookupTables();
+
+    size_t vAlphaSize;
+    size_t expectedSize = 220;
+
+    BOOST_CHECK_EQUAL(vAlphaSize, expectedSize);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorLookupTableCalculateVBetaSize, LookupTableFixture) {
+    QuantileEstimatorLookupTable table;
+    table.calculateLookupTables();
+
+    size_t vBetaSize;
+    size_t expectedSize = 220;
+
+    BOOST_CHECK_EQUAL(vBetaSize, expectedSize);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorLookupTableCalculateVGammaSize, LookupTableFixture) {
+    QuantileEstimatorLookupTable table;
+    table.calculateLookupTables();
+
+    size_t vGammaSize;
+    size_t expectedSize = 220;
+
+    BOOST_CHECK_EQUAL(vGammaSize, expectedSize);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorLookupTableCalculateVDeltaSize, LookupTableFixture) {
+    QuantileEstimatorLookupTable table;
+    table.calculateLookupTables();
+
+    size_t vDeltaSize;
+    size_t expectedSize = 220;
+
+    BOOST_CHECK_EQUAL(vDeltaSize, expectedSize);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( QuantileEstimatorTableNonDefaultInitialization, LookupTableFixture ) {
+    QuantileEstimatorLookupTable table(0.25, 0.5, 2.0, 0.5, 1.0);
+
+    BOOST_CHECK_EQUAL(getMesh(), 0.25);
+    BOOST_CHECK_EQUAL(getAlphaMin(), 0.5); 
+    BOOST_CHECK_EQUAL(getAlphaMax(), 2.0);
+    BOOST_CHECK_EQUAL(getBetaMin(), 0.5);
+    BOOST_CHECK_EQUAL(getBetaMax(), 1.0);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
