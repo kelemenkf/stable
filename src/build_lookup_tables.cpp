c5817036512ce9769160656013b839c30be605d3
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-09-23 12:14:35 +0200

    Add build options into build file - mesh, alpha range, betarange, numberofsamples used

--- src/build_lookup_tables.cpp
+++ src/build_lookup_tables.cpp
@@ -3,13 +3,32 @@
 #include "quantile_estimator_lookup_table.hpp"
 
 int main(int argc, char* argv[])
-{
-    QuantileEstimatorLookupTable table(0.1, 0.5, 2.0, 0.0, 1.0);
-    table.calculateLookupTables(static_cast<unsigned int>(std::stoul(argv[1])));
-    table.writeLookupTablesToFile("vAlpha");
-    table.writeLookupTablesToFile("vBeta");
-    table.writeLookupTablesToFile("vGamma");
-    table.writeLookupTablesToFile("vDelta");
+{   
+    if (argc < 2)
+    {
+        QuantileEstimatorLookupTable table(0.1, 0.5, 2.0);
+        table.calculateLookupTables(10000);
+        table.writeLookupTablesToFile("vAlpha");
+        table.writeLookupTablesToFile("vBeta");
+        table.writeLookupTablesToFile("vGamma");
+        table.writeLookupTablesToFile("vDelta");
+    }
+    else
+    {
+        unsigned int numberOfSamples = static_cast<unsigned int>(std::stoul(argv[1]));
+        double mesh =  static_cast<double>(std::stof(argv[2]));
+        double alphaMin = static_cast<double>(std::stof(argv[3]));
+        double alphaMax = static_cast<double>(std::stof(argv[4]));
+        double betaMin = static_cast<double>(std::stof(argv[5]));
+        double betaMax = static_cast<double>(std::stof(argv[6]));
+        
+        QuantileEstimatorLookupTable table(mesh, alphaMin, alphaMax, betaMin, betaMax);
+        table.calculateLookupTables(numberOfSamples);
+        table.writeLookupTablesToFile("vAlpha");
+        table.writeLookupTablesToFile("vBeta");
+        table.writeLookupTablesToFile("vGamma");
+        table.writeLookupTablesToFile("vDelta");
+    }
 
     return 0;
 }
