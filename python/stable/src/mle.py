fc36c108ce1b72d646d9a000e0f2218b93b61ac9
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2025-02-18 15:53:28 +0100

    newton and gradient for MLE class

--- python/stable/src/mle.py
+++ python/stable/src/mle.py
@@ -6,14 +6,53 @@ from .density import StableDensity
 
 class MLE(Stable):
     def __init__(self, X, alpha, beta, gamma, delta):
-        self.X = X 
+        self.X = np.sort(np.array(X))
         self.alpha = alpha 
         self.beta = beta
         self.gamma = gamma 
         self.delta = delta
 
 
-    def loglikelihood(self):
-        logL_initial = 0
-        pdf_initial = StableDensity(self.X, self.alpha, self.beta, self.gamma)
+    def loglikelihood(self, params):
+        alpha, beta, gamma, delta = params 
+        pdf = StableDensity(self.X, alpha, beta, gamma, delta).get_pdf()
+
+        logL = np.sum(np.log(pdf))
+
+        return logL 
+
+
+    def gradient(self, eps=10e-5):
+        theta = np.array([self.alpha, self.beta, self.gamma, self.delta])
+
+        grad = []
+
+        for t in range(theta.size):
+            forward = theta.copy()
+            backward = theta.copy()
+            forward[t] += eps
+            backward[t] -= eps
+            df_dx = (self.loglikelihood(forward) - self.loglikelihood(backward))  /  (2 * eps)
+            grad.append(df_dx)
+
+        return np.array(grad)
+
+    
+    def hessian(self):
+        pass
+
+
+    def newton(self, eps=10e-6):
+        while (np.linalg.norm(G , 2) > eps):
+            G = self.gradient()
+
+            if np.linalg.norm(G, 2) < eps: 
+                break
+
+            H = self.hessian()
+            x_k = np.array([self.alpha, self.beta, self.gamma, self.delta])
+            H_inverse = np.linalg.inv(H)
+
+            x_k = x_k - (H_inverse @ G)
+            self.alpha, self.beta, self.gamma, self.delta = x_k
 
