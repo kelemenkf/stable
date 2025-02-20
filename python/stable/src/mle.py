7315fa20ac05cc9b874a7dc60bd216cc39428296
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2025-02-20 14:22:14 +0100

    MLE MVP but faster than scipy

--- python/stable/src/mle.py
+++ python/stable/src/mle.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
+import time
 from .stable import Stable
 from .density import StableDensity
+
 
 
 class MLE(Stable):
@@ -11,10 +13,15 @@ class MLE(Stable):
         self.beta = beta
         self.gamma = gamma 
         self.delta = delta
+        self.newton()
+
+    
+    def get_params(self):
+        return (self.alpha, self.beta, self.gamma, self.delta)
 
 
     def loglikelihood(self, params):
-        alpha, beta, gamma, delta = params 
+        alpha, beta, gamma, delta = params
         pdf = StableDensity(self.X, alpha, beta, gamma, delta).get_pdf()
 
         logL = np.sum(np.log(pdf))
@@ -22,7 +29,9 @@ class MLE(Stable):
         return logL 
 
 
-    def gradient(self, eps=10e-5):
+    def gradient(self, eps=10e-6):
+        start = time.time()
+
         theta = np.array([self.alpha, self.beta, self.gamma, self.delta])
 
         grad = []
@@ -35,24 +44,79 @@ class MLE(Stable):
             df_dx = (self.loglikelihood(forward) - self.loglikelihood(backward))  /  (2 * eps)
             grad.append(df_dx)
 
+        end = time.time()
+
+        print("Gradient ", end - start)
+
         return np.array(grad)
 
     
-    def hessian(self):
-        pass
+    def hessian(self, eps=10e-6):
+        start = time.time()
+
+        theta = np.array([self.alpha, self.beta, self.gamma, self.delta])
+        f_theta = self.loglikelihood(theta)
+
+        hess = np.zeros((theta.size, theta.size))
+
+        for i in range(theta.size):
+            for j in range(theta.size):
+                if i == j:
+                    forward = theta.copy()
+                    backward = theta.copy()
+                    forward[j] += eps
+                    backward[j] -= eps
+                    df2_dx2 = (self.loglikelihood(forward) - (2 * f_theta) + self.loglikelihood(backward)) / (eps**2)
+                    hess[i, j] = df2_dx2
+                else:
+                    theta_ij1 = theta.copy()
+                    theta_ij2 = theta.copy()
+                    theta_ij3 = theta.copy()
+                    theta_ij4 = theta.copy()
+
+                    theta_ij1[i] += eps
+                    theta_ij1[j] += eps
+
+                    theta_ij2[i] += eps
+                    theta_ij2[j] -= eps
+
+                    theta_ij3[i] -= eps
+                    theta_ij3[j] += eps
+
+                    theta_ij4[i] -= eps
+                    theta_ij4[j] -= eps
+
+                    f1 = self.loglikelihood(theta_ij1)
+                    f2 = self.loglikelihood(theta_ij2)
+                    f3 = self.loglikelihood(theta_ij3)
+                    f4 = self.loglikelihood(theta_ij4)
+
+                    hess[i, j] = (f1 - f2 - f3 + f4) / (4 * eps ** 2)  
+        
+        end = time.time()
+
+        print("Hessian ", end - start)
+        
+        return hess
 
 
     def newton(self, eps=10e-6):
+        G = self.gradient()
+
         while (np.linalg.norm(G , 2) > eps):
             G = self.gradient()
+
+            print(G)
 
             if np.linalg.norm(G, 2) < eps: 
                 break
 
             H = self.hessian()
             x_k = np.array([self.alpha, self.beta, self.gamma, self.delta])
+            start = time.time()
             H_inverse = np.linalg.inv(H)
-
+            end = time.time()
+            print("Inverse ", end - start)
             x_k = x_k - (H_inverse @ G)
             self.alpha, self.beta, self.gamma, self.delta = x_k
 
