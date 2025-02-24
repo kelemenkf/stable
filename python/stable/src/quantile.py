90fb61c756c5ab424de4cfc39bb772cda1c0c406
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2025-02-24 15:10:35 +0100

    quantile gives estimates

--- python/stable/src/quantile.py
+++ python/stable/src/quantile.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+from scipy.interpolate import RegularGridInterpolator
+from scipy.optimize import minimize
 sys.path.append("/Users/kelemenkf/dev/stable/cpp/build/src")
 import stable_cpp
 
@@ -10,8 +12,15 @@ class Quantile():
       self.v_alpha = self.calculate_v_alpha()
       self.v_beta = self.calculate_v_beta()
       self.v_gamma = self.calculate_v_gamma()
-      self.alpha_values, self.alphas = self.load_alpha_table()
-      self.beta_values, self.betas = self.load_beta_table()
+      self.v_delta = self.calculate_v_delta()
+      self.v_alpha_values, self.alphas = self.load_alpha_table()
+      self.v_beta_values, self.betas = self.load_beta_table()
+      self.v_gamma_values = self.load_gamma_table()
+      self.v_delta_values = self.load_delta_table()
+      self.alpha, self.beta = self.estimate_alpha_beta()
+      print(self.v_gamma_values, self.alphas, self.betas)
+      self.gamma = self.estimate_gamma()
+      self.delta = self.estimate_delta()
 
 
     def calculate_v_alpha(self):
@@ -40,9 +49,67 @@ class Quantile():
         return beta_table[1:,1:], beta_table[0,1:]
     
 
-    def estimate_gamma(self):
-        pass
+    def load_gamma_table(self):
+        gamma_table = np.loadtxt("/Users/kelemenkf/dev/stable/cpp/assets/vGamma_lookup_tables.csv", delimiter=',')
+        return gamma_table[1:,1:]
+
 
     
+    def load_delta_table(self):
+        delta_table = np.loadtxt("/Users/kelemenkf/dev/stable/cpp/assets/vDelta_lookup_tables.csv", delimiter=',')
+        return delta_table[1:,1:]
+
+
+    def v_alpha_interpolator(self):
+        v = RegularGridInterpolator((self.alphas, self.betas), self.v_alpha_values)
+        return v 
+
+
+    def v_beta_interpolator(self):
+        v = RegularGridInterpolator((self.alphas, self.betas), self.v_beta_values)
+        return v 
+    
+
+    def v_gamma_interpolator(self):
+        v = RegularGridInterpolator((self.alphas, self.betas), self.v_gamma_values)
+        return v
+
+
+    def v_delta_interpolator(self):
+        v = RegularGridInterpolator((self.alphas, self.betas), self.v_delta_values)
+        return v
+    
+ 
+    def estimate_alpha_beta(self):
+        v_alpha = self.v_alpha_interpolator()
+        v_beta = self.v_beta_interpolator()
+
+        def error_function(vars):
+            alpha, beta = vars
+            err1 = (v_alpha((alpha, beta)) - self.v_alpha) ** 2
+            err2 = (v_beta((alpha, beta)) - self.v_beta) ** 2
+            return err1 + err2
+
+        initial_guess = [np.mean(self.alphas), np.mean(self.betas)]  
+
+        result = minimize(error_function, initial_guess, bounds=[(min(self.alphas), max(self.alphas)), (min(self.betas), max(self.betas))])
+        
+        return result.x if result.success else None  
+
+
+    def estimate_gamma(self):
+        v_gamma = self.v_gamma_interpolator()
+
+        v_gamma_1 = v_gamma([self.alpha, self.beta])[0]
+        gamma = self.v_gamma / v_gamma_1
+
+        return gamma
+
+
     def estimate_delta(self): 
-        pass
+        v_delta = self.v_delta_interpolator()
+
+        v_delta_1 = v_delta([self.alpha, self.beta])[0]
+        delta = self.v_delta / v_delta_1
+
+        return delta 
